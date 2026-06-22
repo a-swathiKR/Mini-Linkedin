5a6077e2816415d617132ce8d6ea0935438ca393
Aswathi <aswathikr528@gmail.com>
2026-06-22 20:58:32 +0530

    Initial project setup

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const path = require("path");
+const User = require("./Models/User");
+
+main()
+    .then((result) => {
+        console.log("conection successful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/linkedin');
+}
+
+app.set("view engine", "ejs");
+app.set("Views", path.join(__dirname, "Views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+})
+
+app.listen("8080", (req, res) => {
+    console.log("listening to the port 8080")
+})
